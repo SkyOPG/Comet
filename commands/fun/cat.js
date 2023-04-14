cca729a2c248cc84f6fb4bf34820f1aa3435449a
NicknameOPG <NicknameOPG1@gmail.com>
2023-04-14 22:59:11 +0000

    added cat command

---
+++ commands/fun/cat.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const axios = require('axios')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('cat')
+    .setDescription('gets cute images of cats with facts'),
+    async execute(client, interaction){
+       const response = await axios.get("https://some-random-api.ml/animal/cat");
+       await interaction.reply({
+        embeds: [
+            new EmbedBuilder()
+            .setTitle('cats')
+            .setDescription(response.data.fact)
+            .setImage(response.data.image)
+        ]
+       })
+    }
+}
