8ef416e2dc4bb905d5b5361278ac853a7a778d39
NicknameOPG <NicknameOPG1@gmail.com>
2023-04-15 15:56:29 +0000

    ping works now ig

--- message/ping.js
+++ message/ping.js
@@ -4,9 +4,16 @@ const { name } = require('../discord.json')
 
 module.exports = {
     data: {
-        name: 'ping'
+        name: 'ping',
+        description: 'returns the bot\'s ping'
     },
     async execute(client, message){
-        await message.reply('my ping is ' + client.ws.ping)
+        await message.reply({ embeds: [
+            new EmbedBuilder()
+            .setTitle(ping)
+            .setDescription('my ping is ' + client.ws.ping)
+            .setFooter(name)
+            .setColor(465724)
+        ]})
     }
 }
