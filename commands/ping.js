d1c59de404314038edb903b7b57bfd8b726b2b8c
SkyOPG <nicknameopg1@gmail.com>
2023-05-02 17:41:07 +0100

    still dosent work

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,4 @@
 const { EmbedBuilder } = require('discord.js')
-const ping = 'ping'
-const { name } = require('../discord.json')
 
 module.exports = {
     data: {
@@ -10,9 +8,8 @@ module.exports = {
     async execute(client, message){
         await message.reply({ embeds: [
             new EmbedBuilder()
-            .setTitle(ping)
+            .setTitle('ping')
             .setDescription('my ping is ' + client.ws.ping)
-            .setFooter(name)
             .setColor(465724)
         ]})
     }
