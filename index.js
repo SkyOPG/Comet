3f16bb8c96445fe774732ee636bdabdaa05ba5a6
SkyOPG <nicknameopg1@gmail.com>
2023-05-02 17:40:37 +0100

    more xhanges to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Database } = require('midb');index
+const { Database } = require('midb');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token, prefix } = require('./config.json');
 
@@ -6,16 +6,11 @@ class BotClient extends Client {
     constructor(options) {
         super(options); 
         this.db = new Database({
-            path: options.database?.path || './db',
-            tables: options.database?.tables || ['main']
-        }).start();
-    }
-}
-
-const client = new BotClient({
-    intents: ['Guilds', 'GuildMessages', 'MessageContent']
-});
-
+            path: './db',
+            tables: ['main']
+        }).start();}}
+        
+const client = new BotClient({intents: ['Guilds', 'GuildMessages', 'MessageContent']});
 require('./interactions')(client)
 require('./events')(client)
 require('./message')(client)
