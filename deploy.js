077c7186b9e73692a519b2a3f680d073e563c27f
SkyOPG <nicknameopg1@gmail.com>
2023-05-06 00:13:43 +0100

    deploy script is now cool too

--- deploy.js
+++ deploy.js
@@ -2,7 +2,9 @@ const { REST, Routes } = require('discord.js');
 const { clientId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const colors = require('colors')
 
+console.log('0-----------| Slash Register'.blue)
 const commands = [];
 const foldersPath = path.join(__dirname, 'interactions');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -14,6 +16,7 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
+			console.log(`Loaded ${file}`.green)
 			commands.push(command.data.toJSON());
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -25,14 +28,14 @@ const rest = new REST().setToken(token);
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${commands.length} application (/) commands.`.yellow);
 
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`.green);
 	} catch (error) {
 		console.error(error);
 	}
