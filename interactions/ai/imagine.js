1b567d772e2eed83447a749b8cf669072657422e
SkyOPG <nicknameopg1@gmail.com>
2023-05-08 17:04:22 +0100

    hidden api key

--- interactions/ai/imagine.js
+++ interactions/ai/imagine.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const prodia = require("prodia-ai");
 const fs = require("fs");
 const fuzzball = require('fuzzball');
+const { prodia } = require('../../config.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,7 +22,7 @@ module.exports = {
             { name: 'AnyV4.5', value: 'anything-v4.5-pruned.ckpt [65745d25]' },
         )),
     async execute(client, interaction){
-    prodia.key("6e5c6577-78d1-48e7-b196-b68455c7c7cd");
+    prodia.key(prodia);
 
     const nsfwWords = fs.readFileSync('./interactions/ai/NSFW.txt', 'utf8').split('\n').map((word) => word.trim().toLowerCase());
     
