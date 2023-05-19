1bcfd6c8e731eddea840c274d46ffca862f82a82
SkyOPG <nicknameopg1@gmail.com>
2023-05-19 20:12:36 +0100

    rewrite at its finest

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 const { token } = require('./config.json');
 const colors = require('colors');
-
+const func = require('./src/funcs/clid')
+const totalLines = func.clid('./src');
 console.log('0--------------| Comet |--------------0'.blue);
+console.log(`Total lines in the project: ${totalLines}`);
 class BotClient extends Client {
     constructor(options) {
         super(options); 
@@ -13,8 +17,8 @@ class BotClient extends Client {
 const client = new BotClient({intents: ['Guilds', 'GuildMessages', 'MessageContent', 'GuildVoiceStates'] });
 
 
-require('./handlers/message')(client)
-require('./handlers/interactions')(client)
-require('./handlers/events')(client)
+require('./src/handlers/message')(client)
+require('./src/handlers/interactions')(client)
+require('./src/handlers/events')(client)
 
 client.login(token);
---
+++ src/events/interactionCreate.js
@@ -0,0 +1,24 @@
+const { Events } = require('discord.js');
+const colors = require('colors')
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(client, interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`.red);
+			return;
+		}
+
+		try {
+			await command.execute(client, interaction);
+		} catch (error) {
+			interaction.reply({ content: `there was an error running this command`, ephemeral: true })
+			console.error(`Error executing ${interaction.commandName}`.red);
+			console.error(error.red)
+		}
+	},
+};
