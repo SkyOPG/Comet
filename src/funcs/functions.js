ed88acf0336936e3d4d8c186cfb7efc421557bda
SkyOPG <nicknameopg1@gmail.com>
2023-05-19 20:45:13 +0100

    e

--- src/funcs/functions.js
+++ src/funcs/functions.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, token } = require('../../config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const colors = require('colors')
 
 module.exports = { 
