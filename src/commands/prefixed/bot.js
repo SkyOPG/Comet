57d505d4a088209bb350ad41a178ebae4f954113
SkyOPG <nicknameopg1@gmail.com>
2023-05-26 01:44:24 +0100

    ye

---
+++ src/commands/prefixed/bot.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder } = require('discord.js')
+
+module.exports = {
+    data: {
+        name: 'bot',
+        aliases: []
+    },
+    async execute(client, message, args){
+        console.log(client)
+        const embed = new EmbedBuilder()
+        .setTitle('Comet Info')
+        .setDescription('Comet is a bot made by thesky#0001')
+        .addFields({ name: 'Slash Commands', value: `\`${client.commands.length}\``, inline: true },
+        { name: 'Prefix Commands', value: `\`${client.cmdsPrefixed.length}\``, inline: true },
+        { name: 'Users', value: `\`${client.users.cache.length}\``, inline: true },
+        { name: 'Guilds', value: `\`${client.guilds.cache.length}\``, inline: true })
+        message.reply({ embeds: [embed] })
+    }
+}
